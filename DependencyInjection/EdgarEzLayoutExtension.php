b2d0349d56bc2afe1b7a0b15f1b3fb966f56ad8b
Emmanuel Drouet <noodle69@users.noreply.github.com>
2015-10-13 15:58:47 +0200

    initialization

---
+++ DependencyInjection/EdgarEzLayoutExtension.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace EdgarEz\LayoutBundle\DependencyInjection;
+
+use Symfony\Component\Config\Resource\FileResource;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * This is the class that loads and manages your bundle configuration
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ */
+class EdgarEzLayoutExtension extends Extension implements PrependExtensionInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+    }
+
+    /**
+     * Loads DemoBundle configuration.
+     *
+     * @param ContainerBuilder $container
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        $configFile = __DIR__ . '/../Resources/config/edgarezlayout.yml';
+        $config = Yaml::parse(file_get_contents($configFile));
+        $container->prependExtensionConfig('ezpublish', $config);
+        $container->addResource(new FileResource($configFile));
+    }
+}
---
+++ EdgarEzLayoutBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace EdgarEz\LayoutBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EdgarEzLayoutBundle extends Bundle
+{
+
+}
